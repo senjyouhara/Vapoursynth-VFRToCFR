57bfc586ed9d0cb3369fde8583e7de97b7a7deb5
senjyouhara <ayano@loli.com.co>
2024-09-25 15:57:47 +0800

    init

--- VFRToCFR.cpp
+++ VFRToCFR.cpp
@@ -1,9 +1,12 @@
+#include "pch.h"
+#include "vfrToCfr.h"
 #include "VapourSynth.h"
-#include "VSHelper.h"
 #include <vector>
 #include <deque>
 #include <fstream>
 #include <string>
+#include<stdio.h>
+
 #define EPSILON 0.0005
 
 struct timestamp {
@@ -13,27 +16,27 @@ struct timestamp {
 };
 
 struct VFRToCFRData {
-	VSNodeRef *node;
+	VSNodeRef* node;
 	VSVideoInfo vi;
 	bool drop;
 	std::vector<unsigned int> frameMap;
 };
 
-static void VS_CC VFRToCFRInit(VSMap *in, VSMap *out, void **instanceData, VSNode *node, VSCore *core, const VSAPI *vsapi) {
-	VFRToCFRData *d = static_cast<VFRToCFRData *>(*instanceData);
+static void VS_CC VFRToCFRInit(VSMap* in, VSMap* out, void** instanceData, VSNode* node, VSCore* core, const VSAPI* vsapi) {
+	VFRToCFRData* d = static_cast<VFRToCFRData*>(*instanceData);
 	vsapi->setVideoInfo(&d->vi, 1, node);
 }
 
-static const VSFrameRef *VS_CC VFRToCFRGetFrame(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
-	VFRToCFRData *d = static_cast<VFRToCFRData *>(* instanceData);
+static const VSFrameRef* VS_CC VFRToCFRGetFrame(int n, int activationReason, void** instanceData, void** frameData, VSFrameContext* frameCtx, VSCore* core, const VSAPI* vsapi) {
+	VFRToCFRData* d = static_cast<VFRToCFRData*>(*instanceData);
 
 	if (activationReason == arInitial) {
 		vsapi->requestFrameFilter(d->frameMap[n], d->node, frameCtx);
 	}
 	else if (activationReason == arAllFramesReady) {
-		const VSFrameRef *src = vsapi->getFrameFilter(d->frameMap[n], d->node, frameCtx);
-		VSFrameRef *dst = vsapi->copyFrame(src, core);
-		VSMap *m = vsapi->getFramePropsRW(dst);
+		const VSFrameRef* src = vsapi->getFrameFilter(d->frameMap[n], d->node, frameCtx);
+		VSFrameRef* dst = vsapi->copyFrame(src, core);
+		VSMap* m = vsapi->getFramePropsRW(dst);
 		vsapi->freeFrame(src);
 		vsapi->propSetInt(m, "_DurationNum", d->vi.fpsDen, paReplace);
 		vsapi->propSetInt(m, "_DurationDen", d->vi.fpsNum, paReplace);
@@ -43,13 +46,13 @@ static const VSFrameRef *VS_CC VFRToCFRGetFrame(int n, int activationReason, voi
 	return nullptr;
 }
 
-static void VS_CC VFRToCFRFree(void *instanceData, VSCore *core, const VSAPI *vsapi) {
-	VFRToCFRData *d = static_cast<VFRToCFRData *>(instanceData);
+static void VS_CC VFRToCFRFree(void* instanceData, VSCore* core, const VSAPI* vsapi) {
+	VFRToCFRData* d = static_cast<VFRToCFRData*>(instanceData);
 	vsapi->freeNode(d->node);
 	delete d;
-}
+};
 
-static void VS_CC VFRToCFRCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi) {
+static void VS_CC VFRToCFRCreate(const VSMap* in, VSMap* out, void* userData, VSCore* core, const VSAPI* vsapi) {
 	VFRToCFRData d{ vsapi->propGetNode(in, "clip", 0, 0), *vsapi->getVideoInfo(d.node) };
 	int err;
 
@@ -69,7 +72,10 @@ static void VS_CC VFRToCFRCreate(const VSMap *in, VSMap *out, void *userData, VS
 
 	unsigned int count{ 0 };
 	std::deque<timestamp> inTimes;
-	std::ifstream file(timecodes);
+	std::fstream file;
+	std::locale::global(std::locale(""));//将全局区域设为操作系统默认区域
+	file.open(timecodes);//可以顺利打开文件了
+	setlocale(LC_ALL, "C");//还原
 	if (!file) {
 		vsapi->setError(out, "Failed to open the timecodes file.");
 		vsapi->freeNode(d.node);
@@ -97,7 +103,7 @@ static void VS_CC VFRToCFRCreate(const VSMap *in, VSMap *out, void *userData, VS
 		}
 	}
 	inTimes.back().end = inTimes.back().start + (inTimes.back().start / (inTimes.size() - 1));
-	
+
 	unsigned int frames = (inTimes.back().start * d.vi.fpsNum / d.vi.fpsDen);
 
 	double check = frames / static_cast<double>(d.vi.fpsNum) * d.vi.fpsDen;
@@ -125,12 +131,12 @@ static void VS_CC VFRToCFRCreate(const VSMap *in, VSMap *out, void *userData, VS
 
 		//Add possible choices.
 		while (!inTimes.empty() && endtime > inTimes.front().start && abs(endtime - inTimes.front().start) > static_cast<double>(EPSILON)) {
-			
+
 			//This if statement can only be false when the framerate from the timecodes
 			//file is really low (unrealistic).
 			if (starttime < inTimes.front().end && abs(starttime - inTimes.front().end) > static_cast<double>(EPSILON)) {
 				choices.push_back(inTimes.front());
-			}			
+			}
 			inTimes.pop_front();
 		}
 
@@ -262,7 +268,7 @@ static void VS_CC VFRToCFRCreate(const VSMap *in, VSMap *out, void *userData, VS
 	vsapi->createFilter(in, out, "Invert", VFRToCFRInit, VFRToCFRGetFrame, VFRToCFRFree, fmParallel, 0, data, core);
 }
 
-VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
+VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin* plugin) {
 	configFunc("blaze.plugin.vfrtocfr", "vfrtocfr", "VFR to CFR Video Converter", VAPOURSYNTH_API_VERSION, 1, plugin);
 	registerFunc("VFRToCFR", "clip:clip;timecodes:data;fpsnum:int;fpsden:int;drop:int:opt;", VFRToCFRCreate, nullptr, plugin);
 }
